2b68d7c465712e7642c2cedb4cc0fc000d877431
Scott Montford <scottmontford@gmail.com>
2020-12-03 15:25:48 +0000

    Logo fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yellowcake',
+    title: 'Moments by Lori',
     siteUrl: 'https://yellowcake.netlify.com'
   },
   plugins: [
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'yellowcake',
-        short_name: 'yellowcake',
+        name: 'momentsbylori',
+        short_name: 'momentsbylori',
         start_url: '/',
         background_color: '#00C2BD',
         theme_color: '#00C2BD',
