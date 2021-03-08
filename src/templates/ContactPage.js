4cf7aea9daf76011dc8308a0e59b1340b2a99756
Scott Montford <scottmontford@gmail.com>
2021-03-08 02:38:44 +0000

    Form fix

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -3,7 +3,7 @@ import { MapPin, Smartphone, Mail } from 'react-feather'
 import { graphql } from 'gatsby'
 
 import PageHeader from '../components/PageHeader'
-import FormSimpleAjax from '../components/FormSimpleAjax'
+import FormSimpleAjax from '../components/FormSimple'
 import Content from '../components/Content'
 import GoogleMap from '../components/GoogleMap'
 import Layout from '../components/Layout'
